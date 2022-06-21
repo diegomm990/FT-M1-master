1af9b1a835d8b1bf8ed071d9329f226eee4559cd
Diego Martinez <103518088+diegomm990@users.noreply.github.com>
2022-06-21 08:45:13 -0300

    TP1 resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let valor = 0;
+  for (let i = 0; i < num.length; i++) {
+    valor += num[i] * (2 ** (num.length - 1 - i));
+  }
+  return valor;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binary = (num % 2).toString();
+  for (; num > 1; ) {
+      num = parseInt(num / 2);
+      binary =  (num % 2) + (binary);
+  }
+  return binary;
 }
 
 
