ef3a7caa5c99a1517f666cf2558eb196ba21d188
Diego Martinez <103518088+diegomm990@users.noreply.github.com>
2022-06-29 10:39:17 -0300

    Ejercicio Resuelto

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let arr = [1];
+  let i = 2;
+  while(num !== 1){
+    if(num % i === 0){
+      arr.push(i);
+      num = num / i;
+    }else{
+      i++;
+    }
+  }
+  return arr;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,20 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let swap = true;
+  while (swap) {
+    swap = false;
+    for(let i=0; i<array.length - 1; i++) {
+      if(array[i] > array[i+1]){
+        let valor1 = array[i];
+        let valor2 = array[i+1];
+        array[i] = valor2;
+        array[i+1] = valor1;
+        swap = true;
+      }
+  }
+}
+return array;
 }
 
 
@@ -23,7 +46,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(var i=1; i<array.length; i++) {
+    let j = i - 1;
+    let valor = array[i];
+    while(j >= 0 && array[j] > valor){ 
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = valor;
+  }
+  return array;
 }
 
 
@@ -32,7 +64,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0; i < array.length - 1 ; i++){
+    let min = i;
+    for(let j = i + 1; j < array.length; j++){
+      if(array[j] < array[min]){
+        min = j;
+      }
+    }
+    let valor = array[i];
+    array[i] = array[min];
+    array[min] = valor;
+  }
+  return array;
 }
 
 
